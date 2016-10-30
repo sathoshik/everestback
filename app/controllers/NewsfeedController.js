003dab3a0ce66bdaf220c8a8a151a9beba51eb04
Zain <zkworld1234@gmail.com>
2016-10-30 11:44:11 -0400

    B-11: Refactored routes and controllers

---
+++ app/controllers/NewsfeedController.js
@@ -0,0 +1,11 @@
+var nfController= this;
+var mongoose= require('mongoose');
+var fs = require('fs');
+var normalizedPath = require("path").join(__dirname, "../models");
+
+//ZKH - Load all of the models
+fs.readdirSync(normalizedPath).forEach(function(filename){
+	if(~filename.indexOf('.js')) require (normalizedPath+"\\"+filename)
+});
+
+module.exports=nfController;
--- server.js
+++ server.js
@@ -6,19 +6,12 @@ var MongoClient = require('mongodb').MongoClient
 var mongoose= require('mongoose');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var router = require('./app/routes/router');
 
 //ZKH - Parse application/json
 app.use(bodyParser.json());
 
-//ZKH - Load all of the models
-fs.readdirSync(__dirname+"/app/models").forEach(function(filename){
-	if(~filename.indexOf('.js')) require (__dirname+"\\app\\models\\"+filename)
-});
-
-//ZKH - Declare all models
-var User= mongoose.model('User');
-var Newsfeed= mongoose.model('Newsfeed');
-var Event= mongoose.model('Event');
+app.use('/', router);
 
 //ZKH - Connection URL
 var url = 'mongodb://localhost:27017/EverestBack';
@@ -32,36 +25,6 @@ mongoose.connect(url, function(err) {
 //ZKH - Connecting the Node server to port 3000
 http.listen(3000, function(){
   console.log('listening on *:3000');
-});
-
-//ZKH - GET
-app.get('/',function(req,res){
-  res.send('Welcome to EverestBack');
-   });
-
-app.get('/getAllUsers', function(req, res){
-	
-	User.find({}, function (err, users){
-		res.send(users);
-	});
-});
-
-//ZKH - POST
-app.post('/addUser',function(req,res){
-	var User= mongoose.model('User');
-	var newUser= new User({
-		Email: req.body.Email,
-		Password: req.body.Password,
-		FirstName: req.body.FirstName,
-		LastName: req.body.LastName
-		
-	});
-	
-	newUser.save(function(err){
-		if(err) throw err;
-	});
-	
-	res.send("Succesfully added a user");
 });
 
 //ZKH - Socket-io Connection
