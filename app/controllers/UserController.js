668f6d6e03c3c7d3c5285a716976af313d7579e9
SathoshiK <sathoshi.k@hotmail.com>
2016-11-06 13:11:27 -0500

    B-20 Initial addition of the user to db

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,36 +1,95 @@
-var userController = this;
-var mongoose = require('mongoose');
+//  Everest_Back
+//
+//  Created by Sathoshi Kumarawadu on 2016-11-06.
+//  Copyright © 2016 Everest. All rights reserved.
+//
+
+require('../models/User');
+
+var imageUploader = require('../helpers/Tools').imageUploader();
 var fs = require('fs');
-var normalizedPath = require("path").join(__dirname, "../models");
+var mongoose = require('mongoose');
 
-//ZKH - Load all of the models
-fs.readdirSync(normalizedPath).forEach(function(filename){
-	if(~filename.indexOf('.js')) require (normalizedPath+"/"+filename)
-});
 
+/**
+ * User mongoose model initializer
+ * @constructor
+ * @param {User}
+ */
 var User = mongoose.model('User');
 
-userController.getAllUsers= function(req, res){
-	User.find({}, function (err, users){
-		res.send(users);
-	});
+
+/**
+ * UserController Initializer
+ * @constructor
+ */
+var UserController = this;
+
+
+UserController.getAllUsers = function (req, res) {
+  User.find({}, function (err, users) {
+    res.send(users);
+  });
 };
 
-userController.addUser=function(req,res){
-	var User= mongoose.model('User');
-	var newUser= new User({
-		Email: req.body.Email,
-		Password: req.body.Password,
-		FirstName: req.body.FirstName,
-		LastName: req.body.LastName
-		
-	});
-	
-	newUser.save(function(err){
-		if(err) throw err;
-	});
-	
-	res.send("Succesfully added a user");
+
+//SKU - Should we safe guard against extra parameters that could be sent?
+/**
+ * Add User object to users collections
+ * @param {request} req, {response} res
+ * @return respond with user._id or error
+ */
+UserController.createNewUser = (req, res) => {
+
+  imageUploader(req, res, (err) => {
+
+    if (err) {
+      res.status(500);
+      res.end(err);
+    }
+
+    //SKU - Initialize User object with associated with mongoose model.
+    var user = new User(req.body);
+    //SKU - If the request does not have email && password, return 500 error.
+    if (user.Email != null && user.Password != null) {
+
+      if (req.files.length > 0) {
+        user.ProfileImageURL = req.files[0].path;
+      } else {
+        //SKU - Reference default image.
+        user.ProfileImageURL = "/public/uploads/Everest1478401348492.jpg";
+      }
+
+      //SKU - Once the image has been uploaded, check if the image is in the correct path. If not, respond with error
+      fs.access(__dirname + "/../../" + user.ProfileImageURL, fs.R_OK | fs.W_OK, (err) => {
+        if (err) {
+          console.log(err);
+          res.end(err.toString());
+        }
+        else {
+          //SKU - Add user object to the users Collection
+          user.save((err) => {
+            if (err) {
+              console.log(err);
+              res.status(500);
+              res.end(err.toString());
+            } else {
+              res.status(200);
+              res.send(user._id);
+            }
+          });
+        }
+      });
+    } else {
+      res.status(500);
+      res.send();
+    }
+  });
 };
 
-module.exports = userController;
+
+/**
+ * Add UserController to global module object
+ * @constructor
+ */
+module.exports = UserController;
--- app/models/User.js
+++ app/models/User.js
@@ -1,17 +1,84 @@
+//  Everest_Back
+//
+//  Created by Sathoshi Kumarawadu on 2016-10-08.
+//  Copyright © 2016 Everest. All rights reserved.
+//
+
+
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 var Schema = mongoose.Schema;
 
-var userSchema= new Schema({
-	"Email" : {type: String, required: true},
-	"Password" : {type: String, required: true},
-	"FirstName" : {type: String, required: true},
-	"LastName" : {type: String, required: true},
-	"ProfileImageURL" : {type: String, default: null},
-	"LastLoginTimestamp" : {type: Date, default:null },
-	"OriginTimestamp" : {type: Date, default:null },
-	"AttendeeEventID" : {type: [Number], default:null },
-	"AdminEventID" :{type: [Number], default:null }
-}); 
 
+/**
+ * Define salt work factor.
+ * Work Factor is defined as the amount of effort (usually measured in units of time)
+   needed to break a cryptosystem.
+ * @benchmark 13 - 0.56773639917374 seconds
+ */
+this.SALT_WORK_FACTOR =  13
+
+
+/**
+ * Define User Schema
+ *
+ */
+var userSchema = new Schema ({
+	"Email" : {type: String, required: true, index: { unique: true } },
+	"Password" : {type: String, required: true },
+	"FirstName" : {type: String, default: null, required: false },
+	"LastName" : {type: String, default: null, required: false },
+	"ProfileImageURL" : {type: String, default: null, required: false },
+	"LastLoginTimestamp" : {type: Date, default:null, required: false },
+	"OriginTimestamp" : {type: Date, default:null, required: false },
+	"AttendeeEventID" : {type: [Number], default:null, required: false },
+	"AdminEventID" :{type: [Number], default:null, required: false }
+});
+
+
+/**
+ * Upon initialization of User, Hash and salt password.
+ *
+ */
+userSchema.pre('save', (next) => {
+  var user = this;
+
+  //SKU - Only hash the password if it has been modified/new
+  if (!user.isModified('Password')) return next();
+
+  //SKU - Generate a salt
+  bcrypt.genSalt(this.SALT_WORK_FACTOR, (err, salt) => {
+    if (err) return next(err);
+
+    //SKU - Hash the password using our new salt
+    bcrypt.hash(user.Password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      //SKU - Override the original password with the hashed one
+      user.Password = hash;
+      next();
+    });
+  });
+});
+
+
+/**
+ * Compare requested password with the salted and hashed password in the db.
+ * @param {string} requestPassword, {function} callBack
+ * @return callBack or isMatch = true/false
+ */
+userSchema.methods.comparePassword = (requestPassword, callBack) => {
+  bcrypt.compare(requestPassword, this.password, (err, isMatch) => {
+    if (err) return callBack(err);
+    callBack(null, isMatch);
+  });
+};
+
+
+/**
+ * Add User Model to global mongoose model object and users collections
+ * @constructor
+ * @param {string} User, {Schema} userSchema
+ */
  mongoose.model('User', userSchema);
 
